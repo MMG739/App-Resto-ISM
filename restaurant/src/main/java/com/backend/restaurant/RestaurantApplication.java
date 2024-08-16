597c033bcee7e0df07ddfb537cfb20db154886d4
Mouhamadou Moustapha Gueye <gueyemouhamadoumoustapha@gmail.com>
2024-08-16 14:35:34 +0000

    Add files via upload

---
+++ restaurant/src/main/java/com/backend/restaurant/RestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.backend.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+		System.out.println("testRestaurant");
+	}
+}
